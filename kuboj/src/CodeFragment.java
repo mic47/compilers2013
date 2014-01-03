f6a193ce0a51d7cc25ec8e08c602572595c0dbe8
kubo <jakub.jursa@relbit.com>
2014-01-03 13:44:46 +0100

    tabs added in output for better readability

--- kuboj/src/CodeFragment.java
+++ kuboj/src/CodeFragment.java
@@ -1,4 +1,5 @@
 public class CodeFragment {
+	public static int TAB_WIDTH = 2;
 	protected String code;
 	protected String register;
 
@@ -25,5 +26,23 @@ public class CodeFragment {
 
 	public String getRegister() {
 		return this.register;
+	}
+	
+	public void addTab() {
+		String[] lines = code.split("\n");
+		String[] tabbedLines = new String[lines.length];
+		String tab = Utils.getTab(TAB_WIDTH);
+		
+		for (int i = 0; i < lines.length; i++) {
+			tabbedLines[i] = tab + lines[i];
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < tabbedLines.length; i++) {
+			sb.append(tabbedLines[i]);
+			sb.append("\n");
+		}
+		
+		code = sb.toString();
 	}
 }
--- kuboj/src/CompilerVisitor.java
+++ kuboj/src/CompilerVisitor.java
@@ -56,6 +56,7 @@ public class CompilerVisitor extends kubojBaseVisitor<CodeFragment> {
 				"<body_code>" + 
 				"}\n"
 		);
+		body.addTab();
 		template.add("body_code", body);
 
 		CodeFragment code = new CodeFragment();
@@ -86,12 +87,14 @@ public class CompilerVisitor extends kubojBaseVisitor<CodeFragment> {
 		ST template = new ST(  
 				"start:\n" + 
 				"<statements_code>" +
-				"br label %end\n" + 
+				Utils.addTab("br label %end\n", CodeFragment.TAB_WIDTH) + 
 				"end:\n" +
 				"<exp_code>" +
-				"ret i32 0\n" // TODO type ... struct ? 
+				Utils.addTab("ret i32 0\n", CodeFragment.TAB_WIDTH) // TODO type ... struct ? 
 		);
+		statements.addTab();
 		template.add("statements_code", statements);
+		exp.addTab();
 		template.add("exp_code", exp);
 
 		CodeFragment code = new CodeFragment();
@@ -109,14 +112,9 @@ public class CompilerVisitor extends kubojBaseVisitor<CodeFragment> {
 	public CodeFragment visitStr(kubojParser.StrContext ctx) {
 		CodeFragment code = new CodeFragment();
 		String s = ctx.STRING().getText();
-		s = s.substring(1, s.length() - 1);
-		s = unescapeJavaString(s);
-		StringBuilder sb = new StringBuilder();
-		for (byte b : s.getBytes()) {
-			sb.append(String.format("\\%02X", b));
-		}
-		sb.append("\\00");
-
+		s = s.substring(1, s.length() - 1); // remove double quotes
+		String hexString = Utils.stringToHex(Utils.unescapeJavaString(s));
+		
 		ST template = new ST(
 				"<reg1> = alloca [<str_size> x i8]\n" + 
 				"store [<str_size> x i8] c\"<hex_str>\", [<str_size> x i8]* <reg1>\n" +
@@ -126,7 +124,7 @@ public class CompilerVisitor extends kubojBaseVisitor<CodeFragment> {
 		String reg2 = generateNewRegister();
 		template.add("reg1", reg1);
 		template.add("reg2", reg2);
-		template.add("hex_str", sb.toString());
+		template.add("hex_str", hexString);
 		template.add("str_size", s.length() + 1);
 
 		code.setRegister(reg2);
@@ -231,90 +229,4 @@ public class CompilerVisitor extends kubojBaseVisitor<CodeFragment> {
         code.addCode(String.format("%s = add i32 0, %s\n", register, value));
         return code;
 	}
-
-
-	/**
-	 * Unescapes a string that contains standard Java escape sequences.
-	 * <ul>
-	 * <li><strong>&#92;b &#92;f &#92;n &#92;r &#92;t &#92;" &#92;'</strong> :
-	 * BS, FF, NL, CR, TAB, double and single quote.</li>
-	 * <li><strong>&#92;X &#92;XX &#92;XXX</strong> : Octal character
-	 * specification (0 - 377, 0x00 - 0xFF).</li>
-	 * <li><strong>&#92;uXXXX</strong> : Hexadecimal based Unicode character.</li>
-	 * </ul>
-	 * 
-	 * @param st
-	 *            A string optionally containing standard java escape sequences.
-	 * @return The translated string.
-	 */
-	public String unescapeJavaString(String st) {
-
-		StringBuilder sb = new StringBuilder(st.length());
-
-		for (int i = 0; i < st.length(); i++) {
-			char ch = st.charAt(i);
-			if (ch == '\\') {
-				char nextChar = (i == st.length() - 1) ? '\\' : st
-						.charAt(i + 1);
-				// Octal escape?
-				if (nextChar >= '0' && nextChar <= '7') {
-					String code = "" + nextChar;
-					i++;
-					if ((i < st.length() - 1) && st.charAt(i + 1) >= '0'
-							&& st.charAt(i + 1) <= '7') {
-						code += st.charAt(i + 1);
-						i++;
-						if ((i < st.length() - 1) && st.charAt(i + 1) >= '0'
-								&& st.charAt(i + 1) <= '7') {
-							code += st.charAt(i + 1);
-							i++;
-						}
-					}
-					sb.append((char) Integer.parseInt(code, 8));
-					continue;
-				}
-				switch (nextChar) {
-				case '\\':
-					ch = '\\';
-					break;
-				case 'b':
-					ch = '\b';
-					break;
-				case 'f':
-					ch = '\f';
-					break;
-				case 'n':
-					ch = '\n';
-					break;
-				case 'r':
-					ch = '\r';
-					break;
-				case 't':
-					ch = '\t';
-					break;
-				case '\"':
-					ch = '\"';
-					break;
-				case '\'':
-					ch = '\'';
-					break;
-					// Hex Unicode: u????
-				case 'u':
-					if (i >= st.length() - 5) {
-						ch = 'u';
-						break;
-					}
-					int code = Integer.parseInt(
-							"" + st.charAt(i + 2) + st.charAt(i + 3)
-							+ st.charAt(i + 4) + st.charAt(i + 5), 16);
-					sb.append(Character.toChars(code));
-					i += 5;
-					continue;
-				}
-				i++;
-			}
-			sb.append(ch);
-		}
-		return sb.toString();
-	}        
 }
