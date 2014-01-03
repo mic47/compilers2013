55097960314431cf10e4221af61ad38beba44f59
kubo <jakub.jursa@relbit.com>
2014-01-03 02:42:21 +0100

    test/hello finally works

--- kuboj/src/library.c
+++ kuboj/src/library.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 
 int writeint(int a) {
-    printf("%d\n", a);
+    printf("%d", a);
     return 0;
 }
 
 int writestr(char *str) {
-    printf("%s\n", str);
+    printf("%s", str);
     return 0;
 }
